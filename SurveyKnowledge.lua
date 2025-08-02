bf7deceb858046e1c147da24225cc31c94a4be98
Tracy Chiappari <tracy.chiappari@gmail.com>
2025-08-02 16:26:31 -0700

    Search inventory and print surveys to chat.

--- SurveyKnowledge.lua
+++ SurveyKnowledge.lua
@@ -1,6 +1,7 @@
 SurveyKnowledge = SurveyKnowledge or {
   name = "SurveyKnowledge",
-  abbreviation = 'SK'
+  abbreviation = 'SK',
+  inventories = {}
 }
 
 -- EVENT HANDLING
@@ -9,6 +10,20 @@ SurveyKnowledge.Handlers = {}
 -- EVENT_PLAYER_ACTIVATED
 SurveyKnowledge.Handlers.OnPlayerLoaded = function(eventCode, initial)
   SurveyKnowledge.Utilities:debug("Player Loaded")
+
+  -- Initialize the inventories
+  SurveyKnowledge.inventories.Bank = SurveyKnowledge.Utilities:getInventory(BAG_BANK)
+  SurveyKnowledge.inventories.Bag = SurveyKnowledge.Utilities:getInventory(BAG_BACKPACK)
+
+  -- Iterate through the inventories and print out the surveys found to chat
+  for type, Inventory in pairs(SurveyKnowledge.inventories) do
+    SurveyKnowledge.Utilities:message("Survey's in " .. type)
+    
+    local list = Inventory:getSurveys()
+    for _, survey in ipairs(list) do
+      SurveyKnowledge.Utilities:message("Profession [" .. survey.profession .. "] Zone [" .. survey.zone .. "]")
+    end
+  end
 end
 
 -- REGISTER EVENTS
--- Utilities.lua
+++ Utilities.lua
@@ -11,3 +11,57 @@ SurveyKnowledge.Utilities.message = function(self, msg)
   end
   self.chat:SetTagColor("A974E3"):Print(string.format("|cA974E3%s|r ",msg))
 end
+
+SurveyKnowledge.Utilities.ucwords = function(self, str)
+  -- Borrowing name from PHP's ucwords function, this will capitalize the first letter of each word in a string
+  -- This is a useful utility, but Lua does not have a built-in function for this.
+  -- The pattern used here is:
+  --  (%a) One alphabetic character
+  --  (%w*) One or more alphanumeric character(s)
+  -- gsub as a global pattern should send each space seperated word to the replace function independently.
+  return str:gsub("(%a)([%w']*)", function(first, rest) 
+    return first:upper() .. rest:lower()
+  end)
+end
+
+SurveyKnowledge.Utilities.format = function(self, str)
+  -- This function will format the string to be in a more readable format, capitalizing the first letter of each word.
+  -- zo_strformat: Formats strings with interpolation, pluralization, localization and many other features.
+  return self:ucwords(zo_strformat("<<1>>", str):lower())
+end
+
+SurveyKnowledge.Utilities.parseSurveyName = function(self, surveyName)
+  -- The expected format is "Profession Survey: Zone Name"
+  local profession, zone = surveyName:match("^(.-) Survey:%s*(.+)$")
+  return profession, zone
+end
+
+-- Constructor function for inventory
+SurveyKnowledge.Utilities.getInventory = function(Utilities, type)
+  local inventory = {}
+
+  inventory.getName = function(self, slot)
+    return GetItemName(type, slot)
+  end
+
+  inventory.getSurveys = function(self)
+    local surveys = {}
+
+    local slot = ZO_GetNextBagSlotIndex(type)
+    while slot do
+      local name = Utilities:format(self:getName(slot))
+      if name:find("survey") ~= nil then
+        local profession, zone = Utilities:parseSurveyName(name)
+        table.insert(surveys,{
+          profession = profession,
+          zone = zone
+        })
+      end
+      slot = ZO_GetNextBagSlotIndex(type, slot)
+    end
+
+    return surveys
+  end
+  
+  return inventory
+end
